37b73d1bcbd75411680c62e5932e8a77ee5095d8
Andrei Baskakov <baskakov8@mail.ru>
2023-07-22 15:39:25 +0400

    Updating tests

--- test_main_page.py
+++ test_main_page.py
@@ -3,11 +3,14 @@ from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    go_to_login_page(browser)
 
 if __name__ == '__main__':
     pytest.main()
