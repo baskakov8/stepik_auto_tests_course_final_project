53edf4c53632d1a494913416279e67e8c864a32b
Andrei Baskakov <baskakov8@mail.ru>
2023-07-23 11:41:44 +0400

    Creating files for pages and adding description of the base class

---
+++ pages/base_page.py
@@ -0,0 +1,8 @@
+class BasePage():
+    '''Description of a base class for all webpages'''
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(url)
