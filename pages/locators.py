6808ad3db833206c3210837b8a84cf1ab980f0db
Andrei Baskakov <baskakov8@mail.ru>
2023-07-23 12:26:47 +0400

    Updating description of locators and tests

---
+++ pages/locators.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators():
+    '''Description of different locators for the main page'''
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
