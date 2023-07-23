83a55c6c49350dc77cb515fca9055f992d4a0b11
Andrei Baskakov <baskakov8@mail.ru>
2023-07-23 11:51:21 +0400

    Creating first variant of Page Object pattern

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,4 +5,4 @@ class BasePage():
         self.url = url
 
     def open(self):
-        self.browser.get(url)
+        self.browser.get(self.url)
--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,13 @@
 import pytest
 from selenium import webdriver
 from selenium.common import NoSuchElementException
-from selenium.webdriver.common.by import By
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
 if __name__ == '__main__':
     pytest.main()
