b6231d0807d028b7f235f59697b64b58013c2b66
Andrei Baskakov <baskakov8@mail.ru>
2023-07-24 10:57:54 +0400

    Adding parametrize for product page's test

--- test_product_page.py
+++ test_product_page.py
@@ -5,11 +5,10 @@ from selenium import webdriver
 from selenium.common import NoSuchElementException
 from .pages.product_page import ProductPage
 
+base_link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-
-def test_guest_should_add_product_to_basket(browser):
-    #link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+@pytest.mark.parametrize('link', [f'{base_link}?promo=offer{i}' if i != 7 else pytest.param(f'{base_link}?promo=offer{i}', marks=pytest.mark.xfail) for i in range(10)])
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.should_add_product_to_basket()
